b69ba7f7528076a02682c2aadfb9ee4365e3b25f
Crowdbotics <team@crowdbotics.com>
2024-03-08 18:48:23 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yellow-darkness-46885",
+  url: "https://yellow-darkness-46885.botics.co",
+  api: "https://yellow-darkness-46885.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
